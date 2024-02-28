d3f46e6b3f63bd426268b216611eaac7f173128f
Rushikesh-Ingole <157869111+Rushikesh-Ingole@users.noreply.github.com>
2024-02-28 18:24:37 +0530

    App.js

---
+++ React_Assignment/Components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+ import header from './Components/header';
+ import videoCard from './Components/videoCard';
+
+function App() {
+  return (
+    <div>
+      <header/>
+      <videoCard />
+    </div>
+  );
+}
+
+
+export default App;
