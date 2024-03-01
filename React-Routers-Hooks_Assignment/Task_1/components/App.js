f97aa1dc1c080b215225e38e43eb757df4cf34ff
Rushikesh-Ingole <157869111+Rushikesh-Ingole@users.noreply.github.com>
2024-03-01 16:36:40 +0530

    Add files via upload

---
+++ React-Routers-Hooks_Assignment/Task_1/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import VideoCard from './components/VedioCard';
+
+
+function App() {
+  return (
+    <div>
+      <VideoCard/>
+      
+    </div>
+  );
+}
+
+
+export default App;
